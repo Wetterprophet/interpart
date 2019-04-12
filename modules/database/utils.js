e493d4cce0834e1daada494d13eaef932730d128
lutzer <in_the_fishtank@gmx.de>
2019-04-12 14:27:16 +0200

    changed utils

--- modules/database/utils.js
+++ modules/database/utils.js
@@ -1,13 +1,15 @@
 /*
  * @Author: Lutz Reiter - http://lu-re.de 
  * @Date: 2019-03-29 19:20:49 
- * @Last Modified by:   Lutz Reiter - http://lu-re.de 
- * @Last Modified time: 2019-03-29 19:20:49 
+ * @Last Modified by: Lutz Reiter - http://lu-re.de
+ * @Last Modified time: 2019-04-11 20:31:57
  */
 
 const _ = require('lodash')
 const util = require('util')
 const { exec } = require('child_process')
+
+const asyncExec = util.promisify(exec);
 
 function addslashes(str) {
     str = str.replace(/\\/g, '\\\\');
@@ -26,7 +28,6 @@ function stripslashes(str) {
 }
 
 async function translate(text, language) {
-    let asyncExec = util.promisify(exec);
     text = addslashes(text)
     language = addslashes(language)
     let result = await asyncExec(`interop-translate --from "${language}" "${text}"`)
