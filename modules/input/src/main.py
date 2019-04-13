41b70e614949b315370aa00b0f0c54bd0ce55cd9
lutzer <in_the_fishtank@gmx.de>
2019-04-13 17:12:32 +0200

    speech detection from mic works now

---
+++ modules/input/src/main.py
@@ -0,0 +1,54 @@
+import random
+import logging
+
+from .keyboardinput import KeyGrabber 
+from .voiceinput import VoiceInput 
+from .statemachine import StateMachine, Action, State
+from .restclient import RestClient
+
+logging.basicConfig(level=logging.INFO)
+
+running = True
+
+def run():
+    global running
+    #initialize
+    logging.info("started script")
+    state = StateMachine()
+    restClient = RestClient("http://localhost:3030")
+
+    while running:
+        if (state.status == State.WAITING_FOR_KEY):
+            # listen to key
+            keyInput = KeyGrabber()
+            language = keyInput.read()
+
+            state.consumeAction(Action.SET_LANGUAGE, language = language)
+        elif state.status == State.FETCH_QUESTION:
+            try:
+                 # fetch question from database
+                questions = restClient.getQuestions(state.language)
+                # pick random questions
+                question = questions[random.randint(0, len(questions) - 1)]
+                state.consumeAction(Action.SET_QUESTION, question = question)
+            except:
+                state.consumeAction(Action.ERROR, error = "Could not fetch question")
+        elif state.status == State.ASKING_QUESTION:
+            logging.info("asking question: " + str(state.question))
+
+            state.consumeAction(Action.DONE)
+        elif state.status == State.LISTENING:
+            # listening for voice input
+            voiceInput = VoiceInput(state.language)
+            answer = voiceInput.listenToMic()
+            state.consumeAction(Action.SEND_ANSWER, answer = answer)
+            
+        elif state.status == State.SENDING:
+            logging.info("answer: " + str(state.answer))
+            stop()
+
+    logging.info("stopped loop")
+
+def stop():
+    global running
+    running = False
